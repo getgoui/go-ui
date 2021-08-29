a4fa7358acf37e26cc916f5132dd4f66ac0c6a1a
Luka Bacic <luka.bacic278@gmail.com>
2021-08-29 12:25:54 +1000

    ci: add commit linter
    install @commitlint/cli, @commitlint/config-conventional, husky and standard-version

---
+++ commitlint.config.js
@@ -0,0 +1,6 @@
+// Reference https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-configuration.md
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  helpUrl: 'https://www.conventionalcommits.org/en/v1.0.0/',
+};
