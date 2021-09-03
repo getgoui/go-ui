2e540915ced68a45bb97d214517e6e63998f9037
Sean Wu <seanwuapps@gmail.com>
2021-09-04 00:34:13 +1000

    chore: clean up test scripts

--- _jest-puppeteer.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  server: {
-    command: 'npm run start-e2e-frame',
-    debug: true,
-  },
-};
--- _jest.e2e.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'jest-puppeteer',
-  testPathIgnorePatterns: ['/node_modules/', 'dist'], //
-  testMatch: ['<rootDir>/src/patterns/**/*.e2e.ts'],
-  globals: {
-    TESTURL: 'http://localhost:3333/',
-  },
-};
