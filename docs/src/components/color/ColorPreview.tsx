d4d628522a3105e062900ec7e163faf837429c1f
Sean Wu <seanwuapps@gmail.com>
2021-12-24 11:22:55 +1100

    style: :art: update colour tokens

--- docs/src/components/color/ColorPreview.tsx
+++ docs/src/components/color/ColorPreview.tsx
@@ -4,7 +4,12 @@ import './ColorPreview.scss';
 
 export default function ColorPreview({ color, textColor, text, isSpecial, onClick }) {
   return (
-    <button type="button" className={`color-preview${isSpecial ? ' special' : ''}`} style={{ backgroundColor: color.rgb().string(), color: textColor }} onClick={onClick}>
+    <button
+      type="button"
+      className={`color-preview${isSpecial ? ' special' : ''}`}
+      style={{ backgroundColor: color.rgb().string(), color: textColor }}
+      onClick={onClick}
+      title={color.hex()}>
       <span>{text}</span>
     </button>
   );
