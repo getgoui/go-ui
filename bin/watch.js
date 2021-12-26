403f1c3881a7e4e3c6b403a8cb110036ac089ff4
Sean Wu <seanwuapps@gmail.com>
2021-12-27 10:38:35 +1100

    chore(cli): :hammer: update start script and watch cli

--- bin/watch.js
+++ bin/watch.js
@@ -9,25 +9,30 @@ import { exec } from 'child_process';
 export default async function watch(args) {
   console.log(chalk.green('Watching for changes...'));
   const rootPath = path.resolve(__dirname, '../');
+  const onGlobalStyleChange = (file) => {
+    console.log(chalk.green(`- ${file} changed`));
+    const saveTarget = path.resolve(rootPath, 'src/global/styles.scss');
+    const data = fs.readFileSync(saveTarget, 'utf8');
+    fs.writeFileSync(saveTarget, data);
+  };
+  const onComponentChange = (file) => {
+    console.log(chalk.green(`- ${file} changed`));
+    console.log(chalk.green(`- Updating readme.md file`));
+    // run stencil build --docs-readme command
+    // @optimisation - find a way to only run doc generation
+    exec('npx stencil build --docs-readme', (err, stdout, stderr) => {
+      console.log(stdout);
+      if (err) {
+        console.error(chalk.red(`exec error: ${err}`));
+        return;
+      }
+      console.log(chalk.green(`- [Result] readme file updated.`));
+    });
+  };
   const watch = {
-    [`${rootPath}/src/global/**/*.scss`]: (file) => {
-      console.log(chalk.green(`- ${file} changed`));
-      const saveTarget = path.resolve(rootPath, 'src/global/styles.scss');
-      const data = fs.readFileSync(saveTarget, 'utf8');
-      fs.writeFileSync(saveTarget, data);
-    },
-    [`${rootPath}/src/components/**/*.tsx`]: (file) => {
-      console.log(chalk.green(`- ${file} changed`));
-      // run stencil build --docs-readme command
-      exec('npx stencil build --docs-readme', (err, stdout, stderr) => {
-        console.log(stdout);
-        if (err) {
-          console.error(chalk.red(`exec error: ${err}`));
-          return;
-        }
-        console.log(chalk.green(`- [Result] readme file updated.`));
-      });
-    },
+    [`${rootPath}/src/global/**/*.scss`]: onGlobalStyleChange,
+    [`${rootPath}/src/components/**/*.tsx`]: onComponentChange,
+    [`${rootPath}/src/patterns/**/*.tsx`]: onComponentChange,
   };
 
   const watchers = [];
