83ab2991e9ce239e47c5a72a04bcdf048c2a3dbb
Sean Wu <seanwuapps@gmail.com>
2021-12-31 15:29:58 +1100

    feat(component): :sparkles: overlay component

--- src/components/go-overlay/go-overlay.tsx
+++ src/components/go-overlay/go-overlay.tsx
@@ -1,6 +1,7 @@
 import { Component, Host, h, Element, Prop, Method } from '@stencil/core';
+import uniqueId from 'lodash.uniqueid';
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 import { getFocusableChildren } from '../../utils/helper';
-
 @Component({
   tag: 'go-overlay',
   styleUrl: 'go-overlay.scss',
@@ -10,12 +11,33 @@ export class GoOverlay {
   @Element() el: HTMLElement;
 
   @Prop({ reflect: true, mutable: true }) active: boolean = false;
+  /**
+   * Type of this overlay, can be dialog or alertdialog
+   */
+  @Prop() type: 'dialog' | 'alertdialog' = 'dialog';
 
+  /**
+   * If true, the overlay will be dismissed when the user clicks outside of it or presses the escape key.
+   */
+  @Prop() dismissible: boolean = true;
+
+  /**
+   * Heading of the overlay content
+   */
+  @Prop() heading?: string;
+
+  // aria-labelledby https://www.w3.org/TR/wai-aria-practices/examples/dialog-modal/dialog.html
+  headingId: string = uniqueId('overlay-label-');
+
+  // for trapping focus
   firstFocusableEl: HTMLElement;
   lastFocusableEl: HTMLElement;
 
   // keep track of the element that triggered the overlay
   originator: HTMLElement;
+
+  // for detecting click outside
+  contentEl: HTMLElement;
 
   componentDidLoad() {
     const focusableChildren = getFocusableChildren(this.el);
@@ -39,21 +61,43 @@ export class GoOverlay {
         }
       }
     });
+
+    // close overlay on click outside
+    if (this.dismissible) {
+      this.el.addEventListener('click', (e: MouseEvent) => {
+        if (this.active) {
+          if (e.target === this.el && e.target !== this.contentEl) {
+            this.close();
+          }
+        }
+      });
+    }
   }
 
   @Method()
   async close() {
+    enableBodyScroll(this.el);
     this.active = false;
     this.originator.focus();
   }
 
   @Method()
   async open() {
+    disableBodyScroll(this.el);
     this.active = true;
     this.originator = document.activeElement as HTMLElement;
-    // const first = getFirstFocusableChild(this.el);
-    // console.log(first);
-    console.log(this.firstFocusableEl);
+
+    // close overlay on escape
+    if (this.dismissible) {
+      document.addEventListener('keydown', (e: KeyboardEvent) => {
+        if (this.active) {
+          if (e.code === 'Escape') {
+            this.close();
+          }
+        }
+      });
+    }
+
     // focus on first focusable element on next tick
     window.requestAnimationFrame(() => {
       this.firstFocusableEl.focus();
@@ -61,10 +105,17 @@ export class GoOverlay {
   }
 
   render() {
-    const { active } = this;
+    const { active, type, heading, headingId } = this;
     return (
-      <Host class={{ active }}>
-        <slot></slot>
+      <Host role={type} aria-modal="true" aria-labelledby={headingId} class={{ active }}>
+        <div class="overlay-content" ref={(el) => (this.contentEl = el)}>
+          <div id={headingId} class="overlay-heading">
+            <slot name="heading">
+              <h3>{heading}</h3>
+            </slot>
+          </div>
+          <slot></slot>
+        </div>
       </Host>
     );
   }
