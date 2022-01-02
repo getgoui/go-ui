34f9c80ae433b93003ef8bf3f49d5be4810c0699
Sean Wu <seanwuapps@gmail.com>
2022-01-02 21:48:30 +1100

    chore: boilerplate code for main nav

--- src/components/go-main-nav/go-main-nav.tsx
+++ src/components/go-main-nav/go-main-nav.tsx
@@ -1,27 +1,61 @@
-import { Component, Host, h, Element } from '@stencil/core';
-  import { inheritAttributes } from '../../utils/helper';
+import { Component, Host, h, Element, Prop } from '@stencil/core';
+import { inheritAttributes } from '../../utils/helper';
 
-  @Component({
-    tag: 'go-main-nav',
-    styleUrl: 'go-main-nav.scss',
-    shadow: false,
-  })
-  export class GoMainNav {
-    @Element() el: HTMLElement;
+export interface INavItem {
+  name: string;
+  url?: string;
+  icon?: string;
+  children?: INavItem[];
+  isCurrent?: boolean;
+}
+@Component({
+  tag: 'go-main-nav',
+  styleUrl: 'go-main-nav.scss',
+  shadow: false,
+})
+export class GoMainNav {
+  @Element() el: HTMLElement;
 
-   
-    // Store attributes inherited from the host element
-    private inheritedAttrs = {};
-    componentWillLoad() {
-      this.inheritedAttrs = inheritAttributes(this.el, ['class', 'style'], false);
-    }
-    
-    render() {
-      return (
-        <Host>
-          <slot></slot>
-        </Host>
-      );
-    }
+  /**
+   * Navigation items to be rendered
+   */
+  @Prop() items: INavItem[];
 
+  // Store attributes inherited from the host element
+  private inheritedAttrs = {};
+  componentWillLoad() {
+    this.inheritedAttrs = inheritAttributes(this.el, ['class', 'style'], false);
   }
+
+  renderNavItem(item: INavItem) {
+    const Tag = item.isCurrent ? 'span': 'a';
+    return (
+      <li>
+        <Tag href={item.url}>
+          {item.icon && <i class={item.icon}></i>}
+          {item.name}
+        </Tag>
+        {item.children && this.renderNavItems(item.children)}
+      </li>
+    )}
+
+  renderNavItems(items: INavItem[]) {
+    return (
+      <ul>
+        {items.map((item) => this.renderNavItem(item))}
+      </ul>
+    );
+  }
+
+  render() {
+    const { items } = this;
+    return (
+      <Host>
+        <nav aria-label="Main navigation" {...this.inheritedAttrs}>
+          {/* render navigation items from prop */}
+          {items && this.renderNavItems(items)}
+        </nav>
+      </Host>
+    );
+  }
+}
