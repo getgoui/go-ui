004c7ae75fd2cd1e22b3cb4036c64b90cf0034be
Sean Wu <seanwuapps@gmail.com>
2022-01-10 16:40:20 +1100

    chore: update dependencies

--- stencil.config.ts
+++ stencil.config.ts
@@ -12,7 +12,7 @@ export const config: Config = {
       type: 'dist',
     },
     {
-      type: 'dist-custom-elements-bundle',
+      type: 'dist-custom-elements',
     },
     {
       type: 'docs-readme',
