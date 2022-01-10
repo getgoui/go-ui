aa740e8dd11d4be83ec612581b30e8366211d8d2
Sean Wu <seanwuapps@gmail.com>
2022-01-11 07:51:58 +1100

    chore: spec for go-nav-drawer

--- src/components/go-nav-drawer/test/go-nav-drawer.spec.tsx
+++ src/components/go-nav-drawer/test/go-nav-drawer.spec.tsx
@@ -1,11 +1,22 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { GoNavDrawer } from '../go-nav-drawer';
 describe('go-nav-drawer', () => {
- it('renders', async () => {
-   const page = await newSpecPage({
-     components: [GoNavDrawer],
-     html: `<go-nav-drawer></go-nav-drawer>`,
-   });
-   expect(page.root).toBeTruthy();
- });
+  it('renders', async () => {
+    const page = await newSpecPage({
+      components: [GoNavDrawer],
+      html: `<go-nav-drawer></go-nav-drawer>`,
+    });
+    expect(page.root).toBeTruthy();
+  });
+  // it('renders items', async () => {
+  //   const page = await newSpecPage({
+  //     components: [GoNavDrawer],
+  //     html: `<go-nav-drawer id="nav" items="[{label: 'yo'}]"></go-nav-drawer>`,
+  //   });
+  //   expect(page.root).toBeTruthy();
+  //   const drawer = page.root.querySelector('#nav') as HTMLGoNavDrawerElement;
+  //   await drawer.open();
+
+  //   expect(drawer.querySelectorAll('.nav-item').length).toBe(1);
+  // });
 });
