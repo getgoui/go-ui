7e5fb3f125eb1ed94c5b599be16a2c1db936ea4d
Sean Wu <seanwuapps@gmail.com>
2022-02-16 21:27:33 +1100

    docs: link

--- src/components/go-link/go-link.tsx
+++ src/components/go-link/go-link.tsx
@@ -1,27 +1,69 @@
-import { Component, Host, h, Element } from '@stencil/core';
-  import { inheritAttributes } from '../../utils/helper';
+import { Component, h, Element, State, Prop, Watch } from '@stencil/core';
+import { inheritAttributes } from '../../utils/helper';
 
-  @Component({
-    tag: 'go-link',
-    styleUrl: 'go-link.scss',
-    shadow: false,
-  })
-  export class GoLink {
-    @Element() el: HTMLElement;
+// 1. check cross domain and add target="_blank" + rel="noopener noreferrer"
+// 2. check target="_blank" and add visual + screen reader queues
+@Component({
+  tag: 'go-link',
+  styleUrl: 'go-link.scss',
+  shadow: false,
+})
+export class GoLink {
+  @Element() el: HTMLElement;
 
-   
-    // Store attributes inherited from the host element
-    private inheritedAttrs = {};
-    componentWillLoad() {
-      this.inheritedAttrs = inheritAttributes(this.el, ['class', 'style'], false);
-    }
-    
-    render() {
-      return (
-        <Host>
-          <slot></slot>
-        </Host>
-      );
-    }
+  /**
+   * The href of the link.
+   */
+  @Prop() href: string;
 
+  // Store attributes inherited from the host element
+  private attrs = {};
+  componentWillLoad() {
+    this.checkExternal();
+    this.attrs = inheritAttributes(this.el, ['href']);
   }
+
+  @State() isExternal: boolean = false;
+
+  @State() opensNewTab: boolean = false;
+
+  @Watch('href')
+  checkExternal() {
+    console.log(this.href);
+    if (!this.href) {
+      return;
+    }
+    this.isExternal = this.isExternalURL(this.href);
+  }
+
+  private isExternalURL(url: string): boolean {
+    if (url.startsWith('#')) {
+      return false;
+    }
+    return new URL(url).origin !== location.origin;
+  }
+
+  render() {
+    const { href, attrs, isExternal } = this;
+
+    let linkAttrs = {
+      ...attrs,
+      href,
+      class: `go-link${attrs['class'] ? attrs['class'] : ''}`,
+    };
+
+    return (
+      <a {...linkAttrs}>
+        <span>
+          <slot></slot>
+        </span>
+        {isExternal ? (
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" class="external-link-icon">
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" />
+          </svg>
+        ) : null}
+      </a>
+    );
+  }
+}
--- src/components/go-link/readme.md
+++ src/components/go-link/readme.md
@@ -1,3 +1,15 @@
 ## go-link API
 
 <!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property | Attribute | Description           | Type     | Default     |
+| -------- | --------- | --------------------- | -------- | ----------- |
+| `href`   | `href`    | The href of the link. | `string` | `undefined` |
+
+
+----------------------------------------------
+
+
