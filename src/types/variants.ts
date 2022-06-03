ff5ebe4caaa6415b3ecd958372d235be489af71c
Sean Wu <seanwuapps@gmail.com>
2022-06-03 12:05:51 +1000

    chore: button variant attribute update

--- src/types/variants.ts
+++ src/types/variants.ts
@@ -1,6 +1,7 @@
 export type ThemeVariants = 'light' | 'dark';
-export type ButtonColorVariants = 'primary' | 'secondary' | 'tertiary' | 'critical';
 export type BannerVariants = 'info' | 'success' | 'critical';
 export type SizeVariants = 'small' | 'medium' | 'large';
 export type Breakpoints = 'mobile' | 'tablet' | 'desktop' | 'large' | 'all';
-export type ChipVariants = 'primary' | 'secondary' | 'success' | 'critical' | 'neutral';
+export type ColorVariants = 'neutral' | 'primary' | 'secondary' | 'success' | 'critical';
+export type ButtonVariants = ColorVariants | 'text';
+export type ChipVariants = ColorVariants;
