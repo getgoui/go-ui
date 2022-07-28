b58511f3fab103c07002dff58f11a0d8067398af
Sean Wu <seanwuapps@gmail.com>
2022-07-29 09:51:06 +1000

    feat: add watch breakpoint util

--- src/utils/match-media.ts
+++ src/utils/match-media.ts
@@ -20,3 +20,20 @@ export function prefersDark(): boolean {
 export function prefersReducedMotion(): boolean {
   return mm('(prefers-reduced-motion: reduce)').matches;
 }
+
+export function watchDevice(callback: (device) => void): void {
+  const syncEl = document.createElement('div');
+  syncEl.setAttribute('id', 'sync-mq');
+  document.body.appendChild(syncEl);
+  console.log('yo', syncEl);
+  var observer = new ResizeObserver(function (entries) {
+    // get device type from body content
+    if (!entries[0]) {
+      return;
+    }
+    const style = getComputedStyle(syncEl, '::before').getPropertyValue('content');
+    callback(style.replace(/"/g, ''));
+  });
+
+  observer.observe(syncEl);
+}
