92c7ca31e46e3776cd8bd787986aeada74d53937
Sean Wu <seanwuapps@gmail.com>
2022-08-19 19:06:38 +1000

    use workspace go-ui dependency in docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,183 +1,189 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-const path = require('path');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const path = require("path");
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'Go UI',
-  tagline: 'A design system for everyone.',
-  url: 'https://go-ui.com',
-  baseUrl: '/',
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/logo.svg',
-  organizationName: 'getgoui', // Usually your GitHub org/user name.
-  projectName: 'go-ui', // Usually your repo name.
+  title: "Go UI",
+  tagline: "A design system for everyone.",
+  url: "https://go-ui.com",
+  baseUrl: "/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/logo.svg",
+  organizationName: "getgoui", // Usually your GitHub org/user name.
+  projectName: "go-ui", // Usually your repo name.
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       docs: {
         sidebar: {
-          hideable: true,
-        },
+          hideable: true
+        }
       },
       navbar: {
-        title: 'Go UI',
+        title: "Go UI",
         logo: {
-          alt: '',
-          src: 'img/logo.svg',
+          alt: "",
+          src: "img/logo.svg"
         },
         items: [
           {
-            type: 'doc',
-            docId: 'guides/getting-started',
-            position: 'left',
-            label: 'Guides',
+            type: "doc",
+            docId: "guides/getting-started",
+            position: "left",
+            label: "Guides"
           },
           {
-            type: 'doc',
-            docId: 'patterns/introduction',
-            position: 'left',
-            label: 'Patterns',
+            type: "doc",
+            docId: "patterns/introduction",
+            position: "left",
+            label: "Patterns"
           },
           {
-            type: 'doc',
-            docId: 'components/introduction',
-            position: 'left',
-            label: 'Components',
+            type: "doc",
+            docId: "components/introduction",
+            position: "left",
+            label: "Components"
           },
           {
-            type: 'doc',
-            docId: 'get-involved/introduction',
-            position: 'left',
-            label: 'Get involved',
+            type: "doc",
+            docId: "get-involved/introduction",
+            position: "left",
+            label: "Get involved"
           },
           {
-            href: 'https://github.com/getgoui/go-ui',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
+            href: "https://github.com/getgoui/go-ui",
+            label: "GitHub",
+            position: "right"
+          }
+        ]
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Patterns',
-                to: '/docs/patterns/introduction',
+                label: "Patterns",
+                to: "/docs/patterns/introduction"
               },
               {
-                label: 'Components',
-                to: '/docs/components/introduction',
-              },
-            ],
+                label: "Components",
+                to: "/docs/components/introduction"
+              }
+            ]
           },
           {
-            title: 'Get involved',
+            title: "Get involved",
             items: [
               {
-                label: 'Bug report',
-                to: '/docs/get-involved/bug-report',
+                label: "Bug report",
+                to: "/docs/get-involved/bug-report"
               },
               {
-                label: 'Feature request',
-                to: '/docs/get-involved/feature-request',
+                label: "Feature request",
+                to: "/docs/get-involved/feature-request"
               },
               {
-                label: 'Development guide',
-                to: '/docs/get-involved/development-guide',
-              },
-            ],
+                label: "Development guide",
+                to: "/docs/get-involved/development-guide"
+              }
+            ]
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Discord',
-                href: 'https://discord.gg/g7cuQAdPfS',
+                label: "Discord",
+                href: "https://discord.gg/g7cuQAdPfS"
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/getgoui',
-              },
-            ],
+                label: "Twitter",
+                href: "https://twitter.com/getgoui"
+              }
+            ]
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: "Blog",
+                to: "/blog"
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/getgoui/go-ui',
-              },
-            ],
-          },
+                label: "GitHub",
+                href: "https://github.com/getgoui/go-ui"
+              }
+            ]
+          }
         ],
         copyright: `
         <small>Built with <a href="https://docusaurus.io/" target="_blank" rel="noopener noreferrer">Docusaurus</a></small>
         <br />
         <small>Illustration by <a href="https://icons8.com/illustrations/author/5c07e68d82bcbc0092519bb6" target="_blank" rel="noopener noreferrer">Icons 8</a> from <a href="https://icons8.com/illustrations" target="_blank" rel="noopener noreferrer">Ouch!</a></small>
-        `,
+        `
       },
       prism: {
         theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        darkTheme: darkCodeTheme
       },
 
       algolia: {
         // The application ID provided by Algolia
-        appId: 'HNTM91YDD1',
+        appId: "HNTM91YDD1",
         // Public API key: it is safe to commit it
-        apiKey: 'ab1e5977dd71b391791fd44a83fe80ef',
-        indexName: 'go-ui',
-      },
+        apiKey: "ab1e5977dd71b391791fd44a83fe80ef",
+        indexName: "go-ui"
+      }
     }),
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
           sidebarCollapsed: false,
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: 'https://github.com/getgoui/go-ui/edit/main/docs/',
-          remarkPlugins: [require('remark-code-import'), require('remark-mermaid-dataurl')],
+          editUrl: "https://github.com/getgoui/go-ui/edit/main/docs/",
+          remarkPlugins: [
+            require("remark-code-import"),
+            require("remark-mermaid-dataurl")
+          ],
           showLastUpdateAuthor: true,
-          showLastUpdateTime: true,
+          showLastUpdateTime: true
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl: 'https://github.com/getgoui/go-ui/edit/main/docs/blog/',
+          editUrl: "https://github.com/getgoui/go-ui/edit/main/docs/blog/"
         },
         theme: {
-          customCss: [require.resolve('./src/css/custom.scss')],
+          customCss: [
+            require.resolve("../packages/core/dist/go-ui/go-ui.css"),
+            require.resolve("./src/css/custom.scss")
+          ]
         },
         googleAnalytics: {
-          trackingID: 'UA-222249493-1',
-        },
-      }),
-    ],
+          trackingID: "UA-222249493-1"
+        }
+      })
+    ]
   ],
   plugins: [
-    'docusaurus-plugin-sass',
-    'docusaurus-plugin-includes',
+    "docusaurus-plugin-sass",
+    "docusaurus-plugin-includes",
     [
-      'docusaurus-plugin-module-alias',
+      "docusaurus-plugin-module-alias",
       {
         alias: {
-          '@/components': path.resolve(__dirname, './src/components'),
-          '@/go-ui': path.resolve(__dirname, '../src/'),
-        },
-      },
-    ],
+          "@/components": path.resolve(__dirname, "./src/components"),
+          "@/go-ui": path.resolve(__dirname, "../packages/core/src")
+        }
+      }
+    ]
   ],
-  stylesheets: ['https://fonts.googleapis.com/icon?family=Material+Icons'],
+  stylesheets: ["https://fonts.googleapis.com/icon?family=Material+Icons"]
 };
