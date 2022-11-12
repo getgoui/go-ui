c57b24c19aa989cc6ca4d1fae53919857e363864
Sean Wu <seanwuapps@gmail.com>
2022-11-12 14:35:23 +1100

    update release pipeline

--- templates/react-vite/src/main.jsx
+++ templates/react-vite/src/main.jsx
@@ -3,8 +3,4 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import '@go-ui/react/dist/go-ui.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
