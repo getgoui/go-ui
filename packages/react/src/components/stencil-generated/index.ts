ad04168b6ad059d3957ca021207568baf5a8a0be
Sean Wu <seanwuapps@users.noreply.github.com>
2022-11-12 14:56:58 +1100

    chore: react build target (#181)
    * Add react wrapper package
    Fixes #180
    
    * react starter template
    
    * update release pipeline

---
+++ packages/react/src/components/stencil-generated/index.ts
@@ -0,0 +1,47 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@go-ui/core';
+
+import { defineCustomElements } from '@go-ui/core/dist/loader';
+
+defineCustomElements();
+export const GoAccordion = /*@__PURE__*/createReactComponent<JSX.GoAccordion, HTMLGoAccordionElement>('go-accordion');
+export const GoAccordionItem = /*@__PURE__*/createReactComponent<JSX.GoAccordionItem, HTMLGoAccordionItemElement>('go-accordion-item');
+export const GoBadge = /*@__PURE__*/createReactComponent<JSX.GoBadge, HTMLGoBadgeElement>('go-badge');
+export const GoBanner = /*@__PURE__*/createReactComponent<JSX.GoBanner, HTMLGoBannerElement>('go-banner');
+export const GoBreadcrumb = /*@__PURE__*/createReactComponent<JSX.GoBreadcrumb, HTMLGoBreadcrumbElement>('go-breadcrumb');
+export const GoButton = /*@__PURE__*/createReactComponent<JSX.GoButton, HTMLGoButtonElement>('go-button');
+export const GoButtonGroup = /*@__PURE__*/createReactComponent<JSX.GoButtonGroup, HTMLGoButtonGroupElement>('go-button-group');
+export const GoCard = /*@__PURE__*/createReactComponent<JSX.GoCard, HTMLGoCardElement>('go-card');
+export const GoCardRow = /*@__PURE__*/createReactComponent<JSX.GoCardRow, HTMLGoCardRowElement>('go-card-row');
+export const GoChip = /*@__PURE__*/createReactComponent<JSX.GoChip, HTMLGoChipElement>('go-chip');
+export const GoContentLayout = /*@__PURE__*/createReactComponent<JSX.GoContentLayout, HTMLGoContentLayoutElement>('go-content-layout');
+export const GoDialog = /*@__PURE__*/createReactComponent<JSX.GoDialog, HTMLGoDialogElement>('go-dialog');
+export const GoDropdown = /*@__PURE__*/createReactComponent<JSX.GoDropdown, HTMLGoDropdownElement>('go-dropdown');
+export const GoFooter = /*@__PURE__*/createReactComponent<JSX.GoFooter, HTMLGoFooterElement>('go-footer');
+export const GoGovAuLogo = /*@__PURE__*/createReactComponent<JSX.GoGovAuLogo, HTMLGoGovAuLogoElement>('go-gov-au-logo');
+export const GoHeaderBar = /*@__PURE__*/createReactComponent<JSX.GoHeaderBar, HTMLGoHeaderBarElement>('go-header-bar');
+export const GoHero = /*@__PURE__*/createReactComponent<JSX.GoHero, HTMLGoHeroElement>('go-hero');
+export const GoIcon = /*@__PURE__*/createReactComponent<JSX.GoIcon, HTMLGoIconElement>('go-icon');
+export const GoInput = /*@__PURE__*/createReactComponent<JSX.GoInput, HTMLGoInputElement>('go-input');
+export const GoLink = /*@__PURE__*/createReactComponent<JSX.GoLink, HTMLGoLinkElement>('go-link');
+export const GoMainNav = /*@__PURE__*/createReactComponent<JSX.GoMainNav, HTMLGoMainNavElement>('go-main-nav');
+export const GoMd = /*@__PURE__*/createReactComponent<JSX.GoMd, HTMLGoMdElement>('go-md');
+export const GoNavDrawer = /*@__PURE__*/createReactComponent<JSX.GoNavDrawer, HTMLGoNavDrawerElement>('go-nav-drawer');
+export const GoNavLink = /*@__PURE__*/createReactComponent<JSX.GoNavLink, HTMLGoNavLinkElement>('go-nav-link');
+export const GoNavList = /*@__PURE__*/createReactComponent<JSX.GoNavList, HTMLGoNavListElement>('go-nav-list');
+export const GoOverlay = /*@__PURE__*/createReactComponent<JSX.GoOverlay, HTMLGoOverlayElement>('go-overlay');
+export const GoProgress = /*@__PURE__*/createReactComponent<JSX.GoProgress, HTMLGoProgressElement>('go-progress');
+export const GoSearchBar = /*@__PURE__*/createReactComponent<JSX.GoSearchBar, HTMLGoSearchBarElement>('go-search-bar');
+export const GoSkipLink = /*@__PURE__*/createReactComponent<JSX.GoSkipLink, HTMLGoSkipLinkElement>('go-skip-link');
+export const GoSpinner = /*@__PURE__*/createReactComponent<JSX.GoSpinner, HTMLGoSpinnerElement>('go-spinner');
+export const GoSwitch = /*@__PURE__*/createReactComponent<JSX.GoSwitch, HTMLGoSwitchElement>('go-switch');
+export const GoTab = /*@__PURE__*/createReactComponent<JSX.GoTab, HTMLGoTabElement>('go-tab');
+export const GoTableWrapper = /*@__PURE__*/createReactComponent<JSX.GoTableWrapper, HTMLGoTableWrapperElement>('go-table-wrapper');
+export const GoTabs = /*@__PURE__*/createReactComponent<JSX.GoTabs, HTMLGoTabsElement>('go-tabs');
+export const GoToTop = /*@__PURE__*/createReactComponent<JSX.GoToTop, HTMLGoToTopElement>('go-to-top');
+export const GoToc = /*@__PURE__*/createReactComponent<JSX.GoToc, HTMLGoTocElement>('go-toc');
+export const GoTooltip = /*@__PURE__*/createReactComponent<JSX.GoTooltip, HTMLGoTooltipElement>('go-tooltip');
