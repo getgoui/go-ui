94fe84b237c40e2e1923c8e589fd6af0b49d96f1
Sean Wu <seanwuapps@gmail.com>
2022-11-27 20:25:46 +1100

    chore: fix test

--- packages/core/src/patterns/go-card-row/test/go-card-row.e2e.ts
+++ packages/core/src/patterns/go-card-row/test/go-card-row.e2e.ts
@@ -1,22 +1,47 @@
 import { newE2EPage } from '@stencil/core/testing';
 
-let html = '';
 describe('go-card-row', () => {
-  // Set up html content
-  beforeAll(async () => {
-    const fs = require('fs');
-    const path = require('path');
-    html = fs.readFileSync(path.resolve(__dirname, '../usage/go-card-row.md'), 'utf8');
-  });
-
   it('renders', async () => {
-    const page = await newE2EPage({ html });
+    const imgSrc = `https://images.hindustantimes.com/img/2022/05/21/550x309/5b88b514-d944-11ec-9c44-6c63284ff8c5_1653164935635.jpg`;
+    const page = await newE2EPage({
+      html: `<div class="container">
+      <go-card-row heading="Dog quotes" more-link-href="#" more-link-text="More quotes">
+        <go-card href="#" card-title="Dog quote" card-subtitle="Subtitle">
+          <img src="${imgSrc}" alt="Poster" slot="media" />
+          Everything I know I learned from dogs
+          <em slot="footer">Nora Roberts (author, The Search)</em>
+        </go-card>
+        <go-card href="#" card-title="Dog quote" card-subtitle="Subtitle">
+          <img src="${imgSrc}" alt="Poster" slot="media" />
+          Dogs do speak, but only to those who know how to listen
+          <em slot="footer">Orhan Pamuk (author, My Name Is Red)</em>
+        </go-card>
+        <go-card href="#" card-title="Dog quote" card-subtitle="Subtitle">
+          <img src="${imgSrc}" alt="Poster" slot="media" />
+          The better I get to know men, the more I find myself loving dogs
+          <em slot="footer">Charles De Gaulle</em>
+        </go-card>
+        <go-card href="#" card-title="Dog quote" card-subtitle="Subtitle">
+          <img src="${imgSrc}" alt="Poster" slot="media" />
+          Everything I know I learned from dogs
+          <em slot="footer">Nora Roberts (author, The Search)</em>
+        </go-card>
+        <go-card href="#" card-title="Dog quote" card-subtitle="Subtitle">
+          <img src="${imgSrc}" alt="Poster" slot="media" />
+          Dogs do speak, but only to those who know how to listen
+          <em slot="footer">Orhan Pamuk (author, My Name Is Red)</em>
+        </go-card>
+        <go-card href="#" card-title="Dog quote" card-subtitle="Subtitle">
+          <img src="${imgSrc}" alt="Poster" slot="media" />
+          The better I get to know men, the more I find myself loving dogs
+          <em slot="footer">Charles De Gaulle</em>
+        </go-card>
+      </go-card-row>
+    </div>
+    `,
+    });
     const element = await page.find('go-card-row');
     expect(element).toHaveClass('hydrated');
-  });
-
-  it('passes automated a11y test', async () => {
-    const page = await newE2EPage({ html });
     await expect(page).toPassA11y();
   });
 });
