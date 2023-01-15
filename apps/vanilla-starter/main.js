b6479f9d5087a60b984bdc1d34c3231c85c713bc
Sean Wu <seanwuapps@gmail.com>
2023-01-15 17:51:49 +1100

    chore: use autoDefineCustomElements

--- apps/vanilla-starter/main.js
+++ apps/vanilla-starter/main.js
@@ -1,5 +1,4 @@
 import '@go-ui/core/dist/go-ui/go-ui.css';
-import { defineCustomElements } from '@go-ui/core/dist/loader';
+import '@go-ui/core';
 import { setupCounter } from './counter.js';
-defineCustomElements();
 setupCounter(document.querySelector('#counter-btn'), document.querySelector('#count'));
