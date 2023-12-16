0812b7a29a4a981e0a233605ee433ba27e519104
Sean Wu <seanwuapps@gmail.com>
2023-12-16 12:25:06 +1100

    add page transition

--- docs/app.vue
+++ docs/app.vue
@@ -2,9 +2,21 @@
   <ClientOnly>
     <AppHeader />
   </ClientOnly>
-  <NuxtPage />
+  <Transition name="fade">
+    <NuxtPage />
+  </Transition>
 </template>
 
-<script lang="ts" setup>
-import { GoButton } from '@go-ui/vue';
-</script>
+<script lang="ts" setup></script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.2s, filter 0.2s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(0.5rem);
+}
+</style>
