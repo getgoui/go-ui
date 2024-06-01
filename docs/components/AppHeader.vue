4c1c05f97502c7e2d2eebdefeda0a4cef09b181c
Sean Wu <seanwuapps@gmail.com>
2024-06-02 08:31:22 +1000

    fix docsite nav position

--- docs/components/AppHeader.vue
+++ docs/components/AppHeader.vue
@@ -1,36 +1,16 @@
 <template>
   <header class="app-header">
-    <GoNavDrawer
-      ref="mobileMenu"
-      label="Mobile"
-      :items="navItems"
-      auto-close
-    ></GoNavDrawer>
+    <GoNavDrawer ref="mobileMenu" label="Mobile" :items="navItems" auto-close></GoNavDrawer>
 
     <GoHeaderBar breakpoint="tablet">
-      <GoButton
-        slot="mobile-menu-trigger"
-        aria-label="menu"
-        icon
-        flat
-        variant="text"
-        @click="handleMobileTriggerClick"
-      >
+      <GoButton slot="mobile-menu-trigger" aria-label="menu" icon flat variant="text" @click="handleMobileTriggerClick">
         <GoIcon decorative icon-set="bx" name="menu" size="1.5rem"></GoIcon>
       </GoButton>
 
       <NuxtLink custom to="/" v-slot="{ href, navigate }">
         <GoGovAuLogo slot="logo" :href="href" @click="navigate">
-          <img
-            slot="main-brand"
-            src="~/assets/img/logo.svg"
-            :alt="siteConfig.name"
-          />
-          <img
-            slot="main-brand-on-dark"
-            src="~/assets/img/logo.svg"
-            :alt="siteConfig.name"
-          />
+          <img slot="main-brand" src="~/assets/img/logo.svg" :alt="siteConfig.name" />
+          <img slot="main-brand-on-dark" src="~/assets/img/logo.svg" :alt="siteConfig.name" />
           <div slot="co-brand">
             <div class="text-size-1">
               <b>{{ siteConfig.name }}</b>
@@ -43,62 +23,30 @@
       </NuxtLink>
 
       <div class="nav-actions" slot="actions">
-        <GoButton
-          v-bind="repoLinkProps"
-          variant="text"
-          icon
-          round
-          flat
-          compact
-          :aria-label="siteConfig.repoLink.label"
-        >
-          <GoIcon
-            decorative
-            size="1.5rem"
-            icon-set="bxl"
-            name="github"
-          ></GoIcon>
+        <GoButton v-bind="repoLinkProps" variant="text" icon round flat compact :aria-label="siteConfig.repoLink.label">
+          <GoIcon decorative size="1.5rem" icon-set="bxl" name="github"></GoIcon>
         </GoButton>
 
         <GoDarkMode ref="themer" @themechange="updateThemeIcon"></GoDarkMode>
 
-        <GoButton
-          variant="text"
-          icon
-          round
-          flat
-          compact
-          @click="toggleDarkMode"
-        >
+        <GoButton variant="text" icon round flat compact @click="toggleDarkMode">
           <GoIcon size="1.5rem" icon-set="bx" :name="themeIcon"></GoIcon>
         </GoButton>
-        <div
-          v-if="siteConfig?.algolia"
-          id="algolia-search"
-          class="algolia"
-        ></div>
+        <div v-if="siteConfig?.algolia" id="algolia-search" class="algolia"></div>
       </div>
 
-      <GoNavBar slot="main-nav" :items="navItems"></GoNavBar>
+      <GoNavBar slot="nav-bar" :items="navItems"></GoNavBar>
     </GoHeaderBar>
   </header>
 </template>
 
 <script lang="ts" setup>
-import {
-  GoNavDrawer,
-  GoHeaderBar,
-  GoButton,
-  GoIcon,
-  GoGovAuLogo,
-  GoDarkMode,
-  GoNavBar,
-} from "@go-ui/vue";
+import { GoNavDrawer, GoHeaderBar, GoButton, GoIcon, GoGovAuLogo, GoDarkMode, GoNavBar } from '@go-ui/vue';
 
 const repoLinkProps = {
-  target: "_blank",
+  target: '_blank',
   href: siteConfig.repoLink.url,
-  rel: "noopener noreferrer nofollow",
+  rel: 'noopener noreferrer nofollow',
 };
 
 const navItems = computed(() => {
@@ -113,22 +61,22 @@ declare global {
 export default defineComponent({
   data() {
     return {
-      currentTheme: "light",
+      currentTheme: 'light',
     };
   },
   computed: {
     themeIcon() {
-      return this.currentTheme === "light" ? "sun" : "moon";
+      return this.currentTheme === 'light' ? 'sun' : 'moon';
     },
   },
   mounted() {
-    if (siteConfig.algolia && typeof docsearch === "function") {
+    if (siteConfig.algolia && typeof docsearch === 'function') {
       const { appId, apiKey, indexName } = siteConfig.algolia;
       docsearch({
         appId,
         apiKey,
         indexName,
-        container: "#algolia-search",
+        container: '#algolia-search',
         debug: false, // Set debug to true if you want to inspect the modal
       });
     }
@@ -138,9 +86,7 @@ export default defineComponent({
       (this.$refs.mobileMenu as any).$el.open();
     },
     toggleDarkMode() {
-      (this.$refs.themer as any).$el.setTheme(
-        this.currentTheme === "light" ? "dark" : "light"
-      );
+      (this.$refs.themer as any).$el.setTheme(this.currentTheme === 'light' ? 'dark' : 'light');
     },
     updateThemeIcon(e: any) {
       this.currentTheme = e.detail.theme;
